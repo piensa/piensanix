274e96a0727c3ddea67f060d349d0284a45d81c1
Ariel Núñez <ingenieroariel@gmail.com>
2018-10-30 15:09:22 -0500

    New configuration.nix

---
+++ configuration.nix
@@ -0,0 +1,50 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ 
+      ./hardware-configuration.nix
+    ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.useOSProber = true;
+
+  networking.hostName = "i";
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "America/Bogota";
+
+  environment.systemPackages = with pkgs; [
+    # server
+    wget vim tmux htop git ripgrep
+    # code
+    firefox kmail vscode vlc
+    # design
+    # blender godot gimp inkscape
+  ];
+
+
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+  hardware.bluetooth.enable = true;
+  services.xserver.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.displayManager.sddm.autoLogin.enable = true;
+  services.xserver.displayManager.sddm.autoLogin.user = "x";
+  services.xserver.desktopManager.plasma5.enable = true;
+
+  users.users.x = {
+    isNormalUser = true;
+    home = "/x";
+    description = "x";
+    extraGroups = ["wheel" "networkmanager"];
+  };
+
+  nixpkgs.config = {
+    allowUnfree = true;
+  };
+
+  system.stateVersion = "18.09";
+}
+
